8af8757343c65b991152a8459ceee38f49cb5c05
craig <craig@hagatorn.com>
2016-01-13 20:58:39 +0000

    implimented lightslider on homepage

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,12 +1,6 @@
 
- // slider elements
- var slides = $('.product-slider li')
-
- // hide all but first
- //slides.slice(1).hide()
- // moved to css
-
- 
-
- // move between objects
- for(slide in slides){}
+  $(document).ready(function() {
+    $(".product-slider").lightSlider({
+	item:1
+	}); 
+  });
