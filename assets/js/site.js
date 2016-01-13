f7444ab382d66c22b92afbd76ac4060cc8f8589c
craig <craig@hagatorn.com>
2016-01-13 19:01:18 +0000

    started to add css and js for annimation of slider

---
+++ assets/js/site.js
@@ -0,0 +1,12 @@
+
+ // slider elements
+ var slides = $('.product-slider li')
+
+ // hide all but first
+ //slides.slice(1).hide()
+ // moved to css
+
+ 
+
+ // move between objects
+ for(slide in slides){}
