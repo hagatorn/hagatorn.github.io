dbbf2e51c944747632a41e156e45393339efd618
craig <craig@hagatorn.com>
2016-01-15 18:26:06 +0000

    improved animation performance. No build up of animations in queue

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,20 +1,30 @@
 
   $(document).ready(function() {
-    $(".product-slider").lightSlider({
-	item:1,
-	auto: true,
-	loop:true,
-	pause: 6000,
-	adaptiveHeight:true
-	});
-    
-    $('.product-slider').hover(
-    	function(){
-    		$('.slider-desc').slideDown();
-    		return false;
-		},
-	function(){
-		$('.slider-desc').slideUp();
-    		return false;
-	}); 
+	$(".product-slider").lightSlider({
+		item:1,
+		auto: true,
+		loop:true,
+		pause: 6000,
+		adaptiveHeight:true
+		});
+	    
+	$('.product-slider').hover(
+	    	function(){
+	    		$('.slider-desc').stop().slideDown();
+	    		return false;
+			},
+		function(){
+			$('.slider-desc').stop().slideUp();
+	    		return false;
+		}); 
+
+	
+	$(".product-gallery").lightSlider({
+		item:1,
+		loop:true,
+		pause: 6000,
+		adaptiveHeight:true,
+		gallery:true,
+		controls:false
+		});
   });
